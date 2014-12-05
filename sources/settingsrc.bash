951753f69f8d1678258b907defe210bb38bda4d2
Evan J. Travis <ejtravis@illinois.edu>
2014-12-05 17:30:00 -0600

    abstracted out settings-specific environment settings.

---
+++ sources/settingsrc.bash
@@ -0,0 +1,25 @@
+#####################################################################
+# Environment Variables
+#####################################################################
+
+export SETTINGS=${HOME}/settings
+export PYLINTRC=${SETTINGS}pylint/pylintrc
+export MYPYLIB=${SETTINGS}/mypylib
+export PYTHONSTARTUP=${MYPYLIB}/pythonstartup.py
+
+export PATH=$PATH:${SETTINGS}
+export PYTHONPATH=${MYPYLIB}
+
+#####################################################################
+# Aliases
+#####################################################################
+
+alias settings.clean="make -f ${SETTINGS}/init/Makefile clean"
+alias settings.init="sourcee;make -f ${SETTINGS}/init/Makefile init;sourcee"
+alias tosettings="cd $SETTINGS"
+
+#####################################################################
+# Favorites
+#####################################################################
+
+
