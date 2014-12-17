fbc5e07cf5c686e3fe5bde53ac0892fa78265d9a
Evan J. Travis <ejtravis@illinois.edu>
2014-12-17 16:41:53 -0600

    Turned out to be a variable ordering issue. Consider revising current organization.

--- sources/all/sourceryrc.bash
+++ sources/all/sourceryrc.bash
@@ -1,20 +1,6 @@
 echo -e "\t${SOURCES}/all/sourceryrc.bash"
 
 #####################################################################
-# Environment Variables
-#####################################################################
-export MYLIBS=${SETTINGS}/mylibs
-export MYPYLIB=${MYLIBS}/pylib
-export MYBASHLIB=${MYLIBS}/bashlib
-
-export PYTHONSTARTUP=${MYPYLIB}/pythonstartup.py
-
-export PATH=$PATH:${SETTINGS}:${MYBASHLIB}
-export PYTHONPATH=$PYTHONPATH:${MYPYLIB}
-
-export VIMRC=${MYHOME}/.vimrc
-
-#####################################################################
 # Aliases
 #####################################################################
 
