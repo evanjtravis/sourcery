39a85aba9aceb862925163dfb4681ace10db3703
Evan J. Travis <ejtravis@illinois.edu>
2014-12-18 13:42:16 -0600

    Improved import structure.

--- mylibs/pylib/pythonstartup.py
+++ mylibs/pylib/pythonstartup.py
@@ -3,5 +3,15 @@
 This file is currently set to the PYTHONSTARTUP environment variable.
 """
 
-from myutils import *
-
+from myutils import c
+from myutils import cd
+from myutils import cdd
+from myutils import cdh
+from myutils import copy_templates
+from myutils import ENV
+from myutils import get_file_contents
+from myutils import ls
+from myutils import pretty_print
+from myutils import print_intro
+from myutils import pwd
+from myutils import q
