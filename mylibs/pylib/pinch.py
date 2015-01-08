ad57da5e31b78731a8d1cef0ac2e4c193c191092
Evan J. Travis <ejtravis@illinois.edu>
2015-01-08 14:22:17 -0600

    begin port of pinch from bash to python.

---
+++ mylibs/pylib/pinch.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import os
+from optparse import OptionParser
+from ConfigParser import ConfigParser
+
+_ARGS = {
+    'DISABLE_LOG': False,
+    'CUT': False,
+    'HELP': False,
+    'VERBOSE': False
+}
+
+
+def usage():
+    pass
+
+
+def check_errors():
+    pass
+
+
+def log():
+    pass
+
+
+def pinch():
+    pass
+
+
+def main():
+    parser = OptionParser()
+    parser.add_option(
+        '-x',
+        '--cut',
+        action='store_true',
+        dest='cut',
+        default=_ARGS['CUT'],
+        help='Delete lines from source file and paste to destination file.'
+    )
+    parser.add_option(
+        '-v',
+        '--verbose',
+        action='store_true',
+        dest='verbose',
+        default=_ARGS['VERBOSE'],
+        help='Execute program while printing important messages.'
+    )
+    parser.add_option(
+        '-d',
+        '--disable-log',
+        action='store_true',
+        dest='disable-log',
+        default=_ARGS['DISABLE_LOG'],
+        help='Disable program logs during execution.'
+    )
+    parser.add_option(
+        '-h',
+        '--help',
+        action='store_true',
+        dest='help',
+        default=_ARGS['HELP'],
+        help='Display this help.'
+    )
+    (options, args) = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
