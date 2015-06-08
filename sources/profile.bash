82bbf143bd64ae2e2bcb0159f0b9890de4b89262
Evan J. Travis <ejtravis@illinois.edu>
2015-06-08 14:12:43 -0500

    Git aware shell

--- sources/profile.bash
+++ sources/profile.bash
@@ -13,6 +13,14 @@ export MYHOME=/home/${MYNAME}
 export SOURCERY=${MYHOME}/sourcery
 export SOURCES=${SOURCERY}/sources
 
+#====================================================================
+# Git Aware Prompt
+#--------------------------------------------------------------------
+export GITAWAREPROMPT=${SOURCES}/utils/git-aware-prompt
+source $GITAWAREPROMPT/main.sh
+export PS1="\n\[$txtgrn\]\u@\h\n\$git_branch\[$txtred\]\$git_dirty\[$txtylw\]@: \w\n\[$txtrst\]\$ "
+#====================================================================
+
 echo -e "Sourced:\n\t${SOURCES}/profile.bash"
 
 source ${SOURCES}/all/main
