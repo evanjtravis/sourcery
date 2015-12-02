7c20864f74ff7cebce17b51e162cdd06efffe013
Evan J. Travis <ejtravis@illinois.edu>
2015-12-02 13:54:18 -0600

    Re-factored sourcing as the service user.

--- sources/profile.bash
+++ sources/profile.bash
@@ -17,14 +17,6 @@ export STATIC=${SOURCERY}/static
 # PROMPT_COMMAND is cleared here and added to later on.
 export PROMPT_COMMAND=""
 
-#====================================================================
-# Git Aware Prompt
-#--------------------------------------------------------------------
-export GITAWAREPROMPT=${SOURCES}/utils/git-aware-prompt
-source $GITAWAREPROMPT/main.sh
-export PS1="\n\[$txtgrn\]\u\[$txtred\]@\[$txtgrn\]\h\n\$git_branch\[$txtred\]\$git_dirty\[$txtylw\]pwd: \w\n\[$txtrst\]\$ "
-#====================================================================
-
 echo -e "Sourced:\n\t${SOURCES}/profile.bash"
 
 source ${SOURCES}/all/main
